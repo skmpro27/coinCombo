41143362446ecfad1f4be82cfb911cfb5a12920c
unknown <kumar.skm03@gmail.com>
2021-02-15 16:42:40 +0530

    added flipCoinCombination.sh

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -4,9 +4,10 @@
 declare -A combo
 declare -A count
 
-combo=(["H"]=0 ["T"]=1 ["HH"]=2 ["TT"]=3 ["HT"]=4 ["TH"]=5)
+combo=(["H"]=0 ["T"]=1 ["HH"]=2 ["TT"]=3 ["HT"]=4 ["TH"]=5 ["HHH"]=6 ["TTT"]=7 ["HHT"]=8 ["HTH"]=9 ["THH"]=10 
+["TTH"]=11 ["THT"]=12 ["HTT"]=13)
 
-for (( i=0; i<6; i+=1 ))
+for (( i=0; i<14; i+=1 ))
 do
 	count[$i]=0
 done
@@ -14,16 +15,16 @@ done
 for (( i=1; i<=20; i+=1 ))
 do
         random=$(( RANDOM%2 ))
-        count[$random]=$(( ${count[$random]}+1 ))
-done
+        count[$random]=$(( ${count[$random]}+1 )) #Singlet
 
-for (( i=1; i<=20; i+=1 ))
-do
         random=$(( RANDOM%4+2 ))
-        count[$random]=$(( ${count[$random]}+1 ))
+        count[$random]=$(( ${count[$random]}+1 )) #Doublet
+
+        random=$(( RANDOM%8+6 ))
+        count[$random]=$(( ${count[$random]}+1 )) #Triplet
 done
 
-for (( i=0; i<6; i+=1 ))
+for (( i=0; i<14; i+=1 ))
 do
 	count[$i]=$(( 5*${count[$i]} ))
 done
